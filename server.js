46f42f3c3f5434e7562a810d34e868222af07952
Roman <roman.comm1@gmail.com>
2022-11-25 16:27:38 +0300

    more deployment changes

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+
+//Install express server
 const express = require('express');
 const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/waity-client'));
-app.get('/*', function (req, res)
-{res.sendFile(path.join(__dirname + '/dist/waity-client/index.html')); });
 
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/Waity-client'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/Waity-client/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
